ae15d27693e13ae14c1b683a4f02c95c36c30047
Ruancarlosba <144129403+Ruancarlosba@users.noreply.github.com>
2025-05-06 10:43:56 -0300

    atualizar

--- projeto/agenda.py
+++ projeto/agenda.py
@@ -1,5 +1,28 @@
-nome = "guilherme"
-idade = 120
+AGENDA = {}
 
-minha_lista = ["azul","amarelo","verde","vermelho","roxo","dourado"]
-print(minha_lista[2:4])
+AGENDA["guilherme"] ={
+    "telefone": "99999928272",
+    "email": "guilherme@solyd.com",
+    "endereco": "av. 1"
+}
+
+AGENDA["maria"] ={
+    "telefone": "888923412",
+    "email": "maria@solyd.com",
+    "endereco": "av. 2"
+}
+
+def mostrar_contatos():
+  for contato in AGENDA:
+   buscar_contato(contato)
+   print("--------------------")
+
+def buscar_contato(contato):
+   print("Nome:",contato)
+   print("telefone:",AGENDA[contato]["telefone"])
+   print("Email:",AGENDA[contato]["email"])
+   print("Edereço:",AGENDA[contato]["endereco"])
+
+mostrar_contatos()  
+
+#buscar_contato('maria')
